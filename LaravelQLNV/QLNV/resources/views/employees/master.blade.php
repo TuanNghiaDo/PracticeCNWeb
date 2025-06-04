630d2ba4550cbd237c33345bceebf5de0b7296a3
TuanNghiaDo <dotuannghia07@gmail.com>
2025-06-05 00:05:59 +0700

    Import boostrap offline

--- LaravelQLNV/QLNV/resources/views/employees/master.blade.php
+++ LaravelQLNV/QLNV/resources/views/employees/master.blade.php
@@ -5,11 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Quản lí nhân viên</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-</head>
+    <link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
 <body>
     <div class="container mt-5">
         @yield('content')
     </div>
+    <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 </body>
 </html>
